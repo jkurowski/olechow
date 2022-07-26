52da068d9b8a38d7809e16abd5de02beb4b564b7
Jacek <kontakt@jk-design.pl>
2022-07-26 10:07:39 +0200

    AOS animations

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -5,14 +5,14 @@
 
             </div>
             <div class="col-12 col-sm-6">
-                <p>Realizacja: <a href="https://www.4dl.pl/">4Dl.pl</a></p>
+                <p>Realizacja: <a href="https://www.4dl.pl/">4DL.pl</a></p>
                 <p>Powered by: <a href="https://www.developro.pl/" target="_blank">DeveloPro.pl</a></p>
             </div>
         </div>
         <div class="row d-flex justify-content-center">
             <div class="col-12 col-xl-8">
                 <img src="{{ asset('/gfx/logo-footer.png') }}" alt="Logo inwestycji Bliski Olechów" class="footer-logo" width="286" height="359">
-                <p>Informujemy, że wszelkie elementy zielone, rzeczy ruchome, wygląd zewnętrzny budynków oraz sposób zagospodarowania nieruchomości i nieruchomości sąsiednich, przedstawione na stronie www.artlocum.pl, mają charakter poglądowy i stanowią jedynie ogólny, przewidywany sposób wykonania inwestycji. Oznacza to, że wizualizacje nie przedstawiają ostatecznych, docelowych właściwości inwestycji wraz z najbliższym otoczeniem i tym samym nie stanowią próbki lub wzoru w rozumieniu art. 5561 § 1k.c. Sposób wykonania inwestycji, w tym zagospodarowania nieruchomości wspólnej/jej części oraz nieruchomości sąsiednich może się w pewnym zakresie zmienić. Szczegółowe i wiążące informacje dotyczące nabywanych nieruchomości zawarte są w umowie przedwstępnej oraz projekcie architektoniczno–budowlanym, który pozostaje do wglądu dla zainteresowanych nabywców w siedzibie oddziału spółki realizującej sprzedaż lokali.</p>
+                <p>Informujemy, że wszelkie elementy zielone, rzeczy ruchome, wygląd zewnętrzny budynków oraz sposób zagospodarowania nieruchomości i nieruchomości sąsiednich, przedstawione na stronie www.bliskiolechow.pl, mają charakter poglądowy i stanowią jedynie ogólny, przewidywany sposób wykonania inwestycji. Oznacza to, że wizualizacje nie przedstawiają ostatecznych, docelowych właściwości inwestycji wraz z najbliższym otoczeniem i tym samym nie stanowią próbki lub wzoru w rozumieniu art. 5561 § 1k.c. Sposób wykonania inwestycji, w tym zagospodarowania nieruchomości wspólnej/jej części oraz nieruchomości sąsiednich może się w pewnym zakresie zmienić. Szczegółowe i wiążące informacje dotyczące nabywanych nieruchomości zawarte są w umowie przedwstępnej oraz projekcie architektoniczno–budowlanym, który pozostaje do wglądu dla zainteresowanych nabywców w siedzibie oddziału spółki realizującej sprzedaż lokali.</p>
             </div>
         </div>
     </div>
