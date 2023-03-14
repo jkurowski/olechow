ed54f96a24365705fb6d6443b1e1e08c1ccc2880
Jacek <kontakt@jk-design.pl>
2023-03-14 03:33:43 +0100

    Poprawka w tekście o bezpieczenstwie na stronie glownej

--- resources/views/front/contact/index.blade.php
+++ resources/views/front/contact/index.blade.php
@@ -11,6 +11,9 @@
 @section('content')
     <div class="container">
         <div class="row">
+            <div class="col-12">
+                <h4 style="color:red">Godziny otwarcia biura w dniach od 13.03.23 od 15.03.23 - 10:00 - 18:00</h4>
+            </div>
             <div class="col-12 col-sm-6 col-lg-4">
                 <div class="contact-box">
                     <div class="contact-icon">
