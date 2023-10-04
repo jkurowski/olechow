d8b8eaa1a8ed08ff41be2bfff9c682f8140069ed
Jacek <kontakt@jk-design.pl>
2023-10-04 10:51:04 +0200

    Dodanie pola opisu do mieszkania

--- resources/views/front/contact/index.blade.php
+++ resources/views/front/contact/index.blade.php
@@ -12,7 +12,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 mb-5">
-                <h4 style="color: red;text-align:center;margin-bottom:0">W dniach 22.09 i 23.09 biuro sprzedaży będzie zamknięte</h4>
+                <h4 style="color: red;text-align:center;margin-bottom:0;display: none">W dniach 22.09 i 23.09 biuro sprzedaży będzie zamknięte</h4>
             </div>
             <div class="col-12 col-sm-6 col-lg-4">
                 <div class="contact-box">
