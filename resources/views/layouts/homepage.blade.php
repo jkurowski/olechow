8f489dcd8b9555358902e500fd86c73180c0cbae
Jacek <kontakt@jk-design.pl>
2025-02-17 17:46:22 +0100

    Zmiana godzin pracy biura

--- resources/views/layouts/homepage.blade.php
+++ resources/views/layouts/homepage.blade.php
@@ -389,7 +389,7 @@
                             <h3>4 pok. <span class="green">od 75</span> m<sup>2</sup></h3>
                         </div>
                     </div>
-                    <a href="{{ route('front.investment.show') }}" class="bttn" data-aos="fade-up" data-aos-offset="300" data-aos-delay="800">ZOBACZ MIESZKANIA</a>
+                    <a href="#" class="bttn" data-aos="fade-up" data-aos-offset="300" data-aos-delay="800">WSZYSTKIE MIESZKANIA SPRZEDANE</a>
                 </div>
             </div>
         </div>
@@ -489,89 +489,6 @@
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="row d-flex justify-content-center mt-4">
-                <div class="col-12 col-xl-7">
-                    @if (session('success'))
-                        <div class="alert alert-success border-0">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                    @if (session('warning'))
-                        <div class="alert alert-warning border-0">
-                            {{ session('warning') }}
-                        </div>
-                    @endif
-                </div>
-                <div class="col-12 col-xl-6">
-                    <form method="post" id="homepage-form" action="{{ route('contact.index') }}" class="validateForm">
-                        {{ csrf_field() }}
-                        <div class="row">
-                            <div class="col-12 form-input">
-                                <label for="form_name">Imię <span class="text-danger">*</span></label>
-                                <input name="form_name" id="form_name" class="validate[required] form-control @error('form_name') is-invalid @enderror" type="text" value="{{ old('form_name') }}">
-
-                                @error('name')
-                                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                                @enderror
-                            </div>
-                            <div class="col-12 form-input col-input-important">
-                                <label for="form_surname">Nazwisko <span class="text-danger">*</span></label>
-                                <input name="form_surname" id="form_surname" class="form-control" type="text" value="{{ old('form_surname') }}">
-                            </div>
-                            <div class="col-12 col-sm-6 form-input">
-                                <label for="form_email">E-mail <span class="text-danger">*</span></label>
-                                <input name="form_email" id="form_email" class="validate[required] form-control @error('form_email') is-invalid @enderror" type="text" value="{{ old('form_email') }}">
-
-                                @error('email')
-                                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                                @enderror
-                            </div>
-                            <div class="col-12 col-sm-6 form-input">
-                                <label for="form_phone">Telefon <span class="text-danger">*</span></label>
-                                <input name="form_phone" id="form_phone" class="validate[required] form-control @error('form_phone') is-invalid @enderror" type="text" value="{{ old('form_phone') }}">
-
-                                @error('phone')
-                                <span class="invalid-feedback" role="alert"><strong>{{ $message }}</strong></span>
-                                @enderror
-                            </div>
-                            <div class="col-12 mt-1 form-input">
-                                <label for="form_message">Treść wiadomości <span class="text-danger">*</span></label>
-                                <textarea rows="5" cols="1" name="form_message" id="form_message" class="validate[required] form-control @error('form_message') is-invalid @enderror">{{ old('form_message') }}</textarea>
-
-                                @error('message')
-                                <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                                @enderror
-                            </div>
-                            <div class="col-12">
-                                <div class="rodo-obligation mt-4">
-                                    <p>Zgodnie z art. 13 ust.1 i ust. 2 Rozporządzenia Parlamentu Europejskiego i Rady (UE) 2016/679 z dnia 27 kwietnia 2016 r. w sprawie ochrony osób fizycznych w związku z przetwarzaniem danych osobowych i w sprawie swobodnego przepływu takich danych informujemy, że administratorem Pani/Pana danych osobowych jest Madey Development spółka z ograniczoną odpowiedzialnością 2 sp.k., z siedzibą w  93-120 Łódź ul. Przybyszewskiego 199/205. Dane będą przetwarzane w celu założenia i prowadzenia konta klienta na stronie internetowej w tym przede wszystkim świadczenia usług drogą elektroniczną jak również w celu komunikacji.</p>
-                                    <p>Osobie, której dane dotyczą, przysługuje prawo dostępu do treści swoich danych oraz ich poprawiania a także prawo sprzeciwu i żądania zaprzestania przetwarzania i usunięcia swoich danych osobowych.. Podanie danych osobowych przez użytkownika jest dobrowolne, jednakże odmowa podania danych osobowych spowoduje  brak możliwości skontaktowania się oraz udzielenia ewentualnej odpowiedzi na treść zamieszczoną w formularzu kontaktowym (w tym celu możesz wysłać takie oświadczenie na adres email biuro@madej-bud.pl lub pisemnie na adres siedziby. (<a href="https://www.madeydevelopment.pl/files/upload/polityka_prywatnosci.pdf" target="_blank">Polityka informacyjna</a>):</p>
-                                </div>
-                            </div>
-                            <div class="rodo-rules">
-                                @foreach ($rules as $r)
-                                    <div class="col-12">
-                                        <div class="rodo-rule clearfix">
-                                            <input name="rule_{{$r->id}}" id="rule_{{$r->id}}" value="1" type="checkbox" @if($r->required === 1) class="validate[required]" @endif data-prompt-position="topLeft:0">
-                                            <label for="zgoda_{{$r->id}}" class="rules-text">{!! $r->text !!}</label>
-                                        </div>
-                                    </div>
-                                @endforeach
-                            </div>
-                        </div>
-                        <div class="row row-form-submit">
-                            <div class="col-12 pt-3">
-                                <div class="input text-center">
-                                    <input name="form_page" type="hidden" value="homepage">
-                                    <button class="bttn" type="submit">WYŚLIJ WIADOMOŚĆ</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
                 </div>
             </div>
         </div>
